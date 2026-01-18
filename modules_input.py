295bd84a857f289c07e8dd60383818230126bc83
pisupati <pisupati@Akhilas-MacBook-Pro-824.local>
2026-01-18 10:06:03 -0800

    Added files on learning how to create a module

---
+++ modules_input.py
@@ -0,0 +1,9 @@
+##Jan 18: Learning to create a module (for my 302 homework...)
+
+#Clear the terminal screen with the 'os' module
+import os
+import modules
+os.system('clear')
+
+modules.nameit("Akhila")
+#object-oriented: use smth inside smth else, just do a period
