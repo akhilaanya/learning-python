0d81e92f34e12f5502d7b3d884e5470fcb9f3abc
Akhila Anya Pisupati <akhilaanya@users.noreply.github.com>
2026-03-13 11:01:37 -0700

    Suppose I haven't updated this repository in a little bit... not learning much Python though anyway. Maybe over break.

---
+++ class_constructors.py
@@ -0,0 +1,22 @@
+#Class constructors and instance initialization
+
+#Instance creation and instance initialization
+
+#Classes can have attributes for storing data and methods for providing behavior
+
+#Object construction or instantiation = creating+initializing objects of a given class
+#Class constructors run the instantiation process
+
+'''instance = ClassName(arg_1,arg_2)'''
+
+class SomeClass():
+    pass
+#Empty = no attributes or methods (pass: placeholder that does nothing)
+
+SomeClass() #new instance of some class
+
+#Rectangle() != r1() (calling a class vs. instance of class)
+#.__call__() to make instance call-able (unrelated)
+
+
+
